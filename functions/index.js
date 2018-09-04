394c9f7dd8e97d80a48cb75431f79a25f1c04f38
gduncan <gduncan@inc-group.co>
2018-09-04 15:09:59 -0500

    new base keep stats

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
 
+const cors = require('cors')({
+  origin: true
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.keepCamRealTime  = functions.https.onRequest((req, res) => {
+  console.log("Dunked");
+  console.log(req);
+
+  
+
+  res.status(200).json({
+    message:"It worked"
+  })
+});
