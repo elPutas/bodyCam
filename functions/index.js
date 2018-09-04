0a22199c4b8b56e6541adf261ec0905e6703599f
gduncan <gduncan@inc-group.co>
2018-09-04 17:12:16 -0500

    lectura xml probado string falta probar parseo

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const cors = require('cors')({
   origin: true
 });
 
+const xml2js = require('xml2js');
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -11,13 +13,41 @@ const cors = require('cors')({
 //  response.send("Hello from Firebase!");
 // });
 
-exports.keepCamRealTime  = functions.https.onRequest((req, res) => {
+exports.keepStats  = functions.https.onRequest((req, res) => {
   console.log("Dunked");
   console.log(req);
 
-  
+  let data = req.rawBody;
+  let xmlData = data.toString();
+  let parser = new xml2js.Parser();
+//https://stackoverflow.com/questions/10904448/node-to-parse-xml-using-xml2js
+  parser.parseString(xml, function(err,result){
+    //Extract the value from the data element
+    extractedData = result['config']['data'];
+    console.log(extractedData);
+    res.status(200).json({
+      message:"It worked",
+      message_bruto: xmlData
+    });
+  });
 
-  res.status(200).json({
-    message:"It worked"
-  })
+
+  /*
+  parseString(xmlData, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).end();
+      return;
+    }
+    res.send(result);
+  */
+  /*
+  parseString(xmlData, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).end();
+      return;
+    }
+    res.send(result);
+    */
 });
