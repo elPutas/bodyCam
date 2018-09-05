613ce04d66c50cce528163b932f32fe147d59859
mambato <you@example.com>
2018-09-04 19:32:31 -0500

    to ge

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,11 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.keepCamRealTime = functions.https.onRequest((req, res)=> {
+  console.log('donke');
+  console.log(req);
+  res.status(200).json({
+    message:'it worked!'
+  });
+});
