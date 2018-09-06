1c067a015fdb0ee9a611f22fba83c2f1bcda640b
gio valdes <giovaldescudriz@gmail.com>
2018-09-06 17:10:40 -0500

    tiempo y camaras

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+// CORS Express middleware to enable CORS Requests.
+const cors = require('cors')({
+  origin: true,
+});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+exports.saveCam = functions.https.onRequest((req, res) => {
+  // [END trigger]
+  // [START sendError]
+  // Forbidding PUT requests.
+  if (req.method === 'PUT') {
+    return res.status(403).send('Forbidden!');
+  }
+  // [END sendError]
+
+  // [START usingMiddleware]
+  // Enable CORS using the `cors` express middleware.
+  return cors(req, res, () => {
+    // [END usingMiddleware]
+    // Reading date format from URL query parameter.
+    // [START readQueryParam]
+
+    // [START sendResponse]
+
+
+
+
+    res.status(200).send();
+    // [END sendResponse]
+  });
+});
+// [END all]
